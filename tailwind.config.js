b9432c0c9de49c79b112927a01cad2991dd0b0ee
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2025-01-05 13:06:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // File dalam src
+    './index.html'           // File HTML di luar src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
